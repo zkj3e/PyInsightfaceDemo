186bfe0c05df9bef60c1c0b40b6a39e9cbcbe32d
zengkejie <zkj3e@githun.com>
2024-05-26 09:32:47 +0800

    add inswapper

---
+++ inswapper_main2.py
@@ -0,0 +1,28 @@
+import datetime
+import numpy as np
+import os
+import os.path as osp
+import glob
+import cv2
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
+
+
+assert insightface.__version__>='0.7'
+
+if __name__ == '__main__':
+    app = FaceAnalysis(name='buffalo_l')
+    app.prepare(ctx_id=0, det_size=(640, 640))
+    swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
+
+    img = cv2.imread('zta.jpg')
+    faces = app.get(img)
+    source_face = faces[0]
+    res = cv2.imread('t5.jpg')
+    for face in faces:
+        print('xxx')
+        res = swapper.get(res, face, source_face, paste_back=True)
+    cv2.imwrite("./zxa_swapped.jpg", res)
+
+
